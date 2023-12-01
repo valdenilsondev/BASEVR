23db6e23a13353b24e409c41713446f55744a215
valdenilson barbosa <valdenilsondev@gmail.com>
2023-12-01 16:06:56 -0300

    Cria interação entre objetos

---
+++ teste/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public Image imageFade;
+	// Use this for initialization
+	void Start () {
+		Invoke("fade", 1);
+	}
+	
+	private void fade()
+    {
+		imageFade.CrossFadeAlpha(0, 2, true);
+	}
+}
---
+++ teste/Assets/Scripts/ReticuleTime.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+public class ReticuleTime : MonoBehaviour {
+
+	public Image imgTime;
+	public float timeTotal;
+	public float timeCorrent;
+	public bool isAtivo;
+
+	public UnityEvent[] timeEvents;
+	public int idEvent;
+	// Use this for initialization
+	void Start () {
+		Time_Exit();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time();
+	}
+	private void time()
+    {
+        if (isAtivo)
+        {
+			timeCorrent += Time.deltaTime;
+			imgTime.fillAmount = timeCorrent/timeTotal;
+			if(timeCorrent >= timeTotal)
+            {
+				isAtivo = false;
+            }
+        }
+    }
+	public void Time_Enter(int _ID)
+	{
+		isAtivo = true;
+		idEvent = _ID;
+    }
+	public void Time_Exit()
+    {
+		isAtivo = false;
+		imgTime.fillAmount = 0;
+		timeCorrent = 0;
+    }
+}
