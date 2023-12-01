104b0d4e496078a37502b5d6fec85c6e8d2b1dfa
valdenilson barbosa <valdenilsondev@gmail.com>
2023-12-01 17:25:16 -0300

    ajustes

---
+++ teste/Assets/Scripts/Canv.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canv : MonoBehaviour {
+
+	public GameObject can;
+	// Use this for initialization
+	void Start () {
+		can.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ca()
+    {
+		can.SetActive(true);
+		Debug.Log("OK");
+    }
+}
